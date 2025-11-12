7a596f7fcdc897b276548a2d8fd71c361e5ed24b
mduc <ngminhduc1412007@gmail.com>
2025-11-13 00:54:51 +0700

    Add files via upload

--- bai lt 8.c
+++ bai lt 8.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 
-int removeElement(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        for (int j = i + 1; j < size; ) {
+int removeElement(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; ) {
             if (arr[i] == arr[j]) {
-                for (int k = j; k < size - 1; k++) {
+                for (int k = j; k < n - 1; k++) {
                     arr[k] = arr[k + 1];
                 }
-                size--; 
-            } else {
+                n--; 
+            }else {
                 j++; 
             }
         }
     }
-    return size;
+    return n;
 }
 
 int main() {
     int arr[] = {1, 2, 3, 2, 4, 5, 1, 6};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    size = removeElement(arr, size);
-    printf("mang sau khi bo phan tu trung lap: \n");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
+    int n = sizeof(arr) / sizeof(arr[0]);
+    n = removeElement(arr, n);
+    printf("mang sau khi bo phan tu trung lap: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
     }
-    printf("kich thuoc cua mang %d", size);
+    printf("kich thuoc cua mang %d", n);
     return 0;
 }
 
